c121d27ac32774b620e5ff1c6493a64912e73546
Ankit Minglani <ankit.minglani@gmail.com>
2021-05-19 19:32:55 +0530

    Update DemoApplication.java
    Making global CORS filter on an spring boot application basis

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -18,11 +21,23 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@CrossOrigin(origins = "http://localhost:4200")
+
+
+	//@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/hello")
 	public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		System.out.println("==== get greeting ====");
 		return new Greeting(counter.incrementAndGet(),  name);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/hello").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
